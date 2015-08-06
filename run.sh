20c9fbb1f0fb25615292169e695169a4521dcc6a
Ian Blenke <ian@blenke.com>
2015-08-06 15:17:13 -0400

    Removing extraneous close parenthesis

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ grep -e '^START_DAEMON=' /etc/default/minissdpd || \
 grep -e "^MiniSSDPd_INTERFACE_ADDRESS=" /etc/default/minissdpd || \
   echo "MiniSSDPd_INTERFACE_ADDRESS=${INTIFACE}" >> /etc/default/minissdpd
 
-sed -i -e "s/^MiniSSDPd_INTERFACE_ADDRESS=.*\$/MiniSSDPd_INTERFACE_ADDRESS=${INTIFACE})/" \
+sed -i -e "s/^MiniSSDPd_INTERFACE_ADDRESS=.*\$/MiniSSDPd_INTERFACE_ADDRESS=${INTIFACE}/" \
        -e "s/^START_DAEMON=.*\$/START_DAEMON=1/" \
        /etc/default/minissdpd
 
